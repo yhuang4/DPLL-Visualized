bc4d40ed46fc0b809dc90626847477b1777e1e03
Ethan Huang <yhuang4@andrew.cmu.edu>
2023-12-17 00:26:17 +0800

    refactor and tested

--- main.py
+++ main.py
@@ -1,221 +1,149 @@
-# while True:
-#     sat_input = input().split()
-#     if len(sat_input) == 4 and sat_input[0] == 'p' and sat_input[1] == 'cnf':
-#         try:
-#             nbvar, nbclauses = int(sat_input[2]), int(sat_input[3])
-#             break
-#         except:
-#             pass
-#     print('Fomat error. Format: p cnf [nbvar] [nbclauses]')
 from input import read_cnf_file
 import copy
-import os
 import sys
-sys.setrecursionlimit(10000)
-# nbvar, nbclauses = map(int, input().split())
 
-# F = []
-# A = [-1] * (nbvar + 1)
-# for i in range(nbclauses):
-#     clause = list(map(int, input().split()))[:-1]  # Terminated by a 0
-#     F.append(clause)
+class State():
+    def __init__(self, F, A, implied):
+        self.F = F
+        self.A = copy.deepcopy(A)
+        self.F_A = None
+        self.implied = copy.deepcopy(implied)
 
-nbvar, nbclauses, F = read_cnf_file('UUF50.218.1000/uuf50-01.cnf')
-# nbvar, nbclauses, F = read_cnf_file('cnfs/uf20-0744.cnf')
-
-assignments = []
-implied = []
-A = [-1] * (nbvar + 1)
-F_test = copy.deepcopy(F)
-
-def get_unit():
-    for i, clause in enumerate(F):
-        unassigned_count = 0
-        unassigned = 0
-        unsat = True
-        for lit in clause:
-            # If the clause is satisfied, break
-            if lit > 0 and A[lit] == 1:
-                unsat = False
-                break
-            elif lit < 0 and A[abs(lit)] == 0:
-                unsat = False
-                break
-            
-            # Start checking for unassigned
-            if A[abs(lit)] == -1:
-                unassigned_count += 1
-                if unassigned_count > 1:
+    def eval_helper(self, to_eval):
+        new_F_A = []
+        for i, clause in to_eval:
+            new_clause = []
+            for lit in clause:
+                if (lit > 0 and self.A[lit] == 1) or (lit < 0 and self.A[-lit] == -1): # lit is true
+                    new_clause = None
                     break
-                unassigned = lit
-        if unsat and unassigned_count == 1:
-            # implied[abs(unassigned)] = i
-            implied.append((abs(unassigned), i))
-            return unassigned
-    return 0
+                elif self.A[abs(lit)] == 0:  # not assigned
+                    new_clause.append(lit)
+            if new_clause != None:
+                new_F_A.append((i, new_clause))
+        
+        self.F_A = new_F_A
 
+    def force_eval(self):
+        self.eval_helper(self.F)
 
-def all_sat():
-    for clause in F:
-        sat = False
-        for lit in clause:
-            if lit > 0 and A[lit] == 1:
-                sat = True
-                break
-            if lit < 0 and A[abs(lit)] == 0:
-                sat = True
-                break
-        if not sat:
-            return False
-    return True
-
-def get_conflict():
-    # print('A:', A)
-    count = 0
-    for clause in F:
-        # print('clause:', clause)
-        count += 1
-        if len(clause) == 0: continue # improve logic
-        no_conflict = False
-        for lit in clause:
-            if lit > 0 and A[lit] != 0:
-                no_conflict = True
-                break
-            if lit < 0 and A[abs(lit)] != 1:
-                no_conflict = True
-                break
-        if not no_conflict:
-            return clause
-    # print(count)
-    return None
-
-def learning_procedure(C):
-    C = copy.deepcopy(C)
-    # C.sort(key=lambda x: abs(x), reverse=True)
-    def get_implied():
-        for (x, i) in reversed(implied):
-            if x in C:
-                return (x, i)
-            if -x in C:
-                return (-x, i)
-        # for lit in C:
-        #     if abs(lit) in implied:
-        #         return lit
-        return None
-    
-    def resolve(C_prime, l):
-        C.remove(l)
-        for lit in C_prime:
-            if abs(lit) != abs(l):
-                if -lit in C:
-                    C.remove(-lit)
-                elif lit in C:
-                    C.remove(lit)
-                if lit not in C:
-                    C.append(lit)
-    
-    # print('C:', C)
-    l = get_implied()
-    # print('l:', l)
-    while l != None:
-        # C_prime = F[implied[abs(l)]]
-        l, i = l
-        C_prime = F[i]
-        # print('C:', C)
-        # print('C\':', C_prime)
-        resolve(C_prime, l)
-        l = get_implied()
-        # print('l:', l)
-    # print('learned', C)
-    F.append(C)
-    return C
-
-
-def unit_prop():
-    unit = get_unit()
-    propped_unit = []
-    while unit != 0:
-        if unit > 0:
-            A[unit] = 1
+    def eval(self):
+        if self.F_A == None: 
+            self.eval_helper(self.F)
         else:
-            A[abs(unit)] = 0
-        assignments.append(unit)
-        propped_unit.append(unit)
-        unit = get_unit()
+            self.eval_helper(self.F_A)
 
-def resol_conflict():
-    conflict = get_conflict()
-    # print('conflict:', conflict)
-    if conflict != None:
-        return learning_procedure(conflict)
-    return None
+    def assign_val(self, p):
+        if p > 0:
+            self.A[p] = 1
+        else:
+            self.A[-p] = -1
 
-def choose_atom():
-    # print('A in choose:', A)
-    for clause in F:
-        for lit in clause:
-            if A[abs(lit)] == -1:
-                return abs(lit)
-
-def backtrack(p):
-    i = len(assignments) - 1
-    while i >= 0:
-        x = assignments.pop()
-        j = len(implied) - 1
-        while j >= 0:
-            y, _ = implied[j]
-            if abs(x) == abs(y):
-                implied.pop(j)
-            j -= 1
-        A[abs(x)] = -1
-        if abs(x) == p:
-            break
-
-def proofread():
-    for clause in F_test:
-        sat = False
-        for lit in clause:
-            if lit > 0 and A[lit] == 1:
-                sat = True
-                break
-            elif lit < 0 and A[abs(lit)] == 0:
-                sat = True
-                break
-        if not sat:
-            return False
-    return True
-
-def dpll():
-    # print('F:', F)
-    unit_prop()
-
-    if all_sat():
-        print('sat:', A[1:])
-        print('proofread:', proofread())
-        exit()
+    def sat(self):
+        return len(self.F_A) == 0
     
-    learned = resol_conflict()
-    # print('learned:', learned)
-    # if A[2] == 0: print('F:', F)
-    if learned != None and len(learned) == 0:
-        print('unsat')
-        exit()
-    elif not learned:
-        p = choose_atom()
-        # print(f'assigning {p} to be true')
-        A[p] = 1
-        assignments.append(p)
-        dpll()
-        backtrack(p)
+    def apply_transform(self, p):
+        self.assign_val(p)
+        self.eval()
+    
+    def unit_prop(self):
+        while True:
+            unit_found = False
+            for i, clause in self.F_A:
+                if len(clause) == 1:
+                    unit_found = True
+                    self.apply_transform(clause[0])
+                    self.implied.append((abs(clause[0]), i))
+                    break
 
-        # print(f'assigning {p} to be false')
-        A[p] = 0
-        assignments.append(p)
-        dpll()
-        backtrack(p)
+            if unit_found: continue
+            else: break
+    
 
-dpll()
-print('unsat')
+    def get_conflict(self):
+        for i, clause in self.F_A:
+            if len(clause) == 0:
+                return i
+        return None
 
-# TODO: deal with empty clause
+    def learning_procedure(self, conflict):
+        def most_recent_implied(C):
+            for p, i in reversed(self.implied):
+                if abs(p) in C:
+                    return (p, i)
+            
+            return (None, None)
+        
+        def resolve(C, C_pr, l):
+            for lit in C_pr:
+                if -lit in C:
+                    C.remove(-lit)  # Canceled out by resolution
+                elif lit in C:
+                    pass
+                else:
+                    C.append(lit)
+            
+        C = copy.deepcopy(self.F[conflict][1])
+        while True:  # Loop until all implied literals are replaced
+            l, i = most_recent_implied(C)
+            if l == None:
+                return C
+            C_pr = self.F[i][1]  # C'
+            resolve(C, C_pr, l)
 
+    def choose_atom(self):
+        # Guaranteed to have non empty formula and no empty clause when called
+        return abs(self.F_A[0][1][0])  
+    
+
+def dpll(state):
+    state.unit_prop()
+    
+    if state.sat():
+        return state.A
+    
+    conflict = state.get_conflict()
+
+    if conflict != None:
+        learned_clause = state.learning_procedure(conflict)
+        if len(learned_clause) == 0:
+            print('UNSAT%')
+            exit()
+        elif learned_clause != conflict:
+            state.F.append((len(state.F), learned_clause))
+        return None
+    else:
+        p = state.choose_atom()
+        p_true = State(state.F, state.A, state.implied)
+
+        p_true.apply_transform(p)
+        res_p_true = dpll(p_true)
+        if res_p_true != None:
+            return res_p_true
+
+        p_false = State(state.F, state.A, state.implied)
+        p_false.apply_transform(-p)
+
+        res_p_false = dpll(p_false)
+        return res_p_false
+    
+file_path = sys.argv[1]
+nbvar, nbclauses, F = read_cnf_file(file_path)
+init_F = copy.deepcopy(F)
+F = list(enumerate(F))
+A = [0] * (nbvar + 1)
+S = State(F, A, [])
+S.eval()
+
+res = dpll(S)
+if res != None:
+    res = res[1:]
+    for i in range(len(res)):
+        if res[i] == 0:
+            res[i] = str(i + 1)
+        else:
+            res[i] = str(res[i] * (i + 1))
+    print('SAT')
+    print(" ".join(res) + ' 0%')
+else:
+    print('UNSAT%')
